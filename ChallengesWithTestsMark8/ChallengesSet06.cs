749bb7c7e5131ab8e551684420060215c50138b6
megamagicmonkey <megamagicmonkey@gmail.com>
2021-10-13 15:19:30 -0500

    All tests resolved

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -7,27 +7,129 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            if (words == null)
+            {
+                return false;
+            }
+
+            switch (ignoreCase)
+            {
+                case true:
+                    foreach (var thing in words)
+                    {
+                        if (thing == null)
+                        {
+                            continue;
+                        }
+                        if (thing.ToLower() == word.ToLower())
+                        {
+                            return true;
+                        }
+                    }
+                    return false;
+                case false:
+                    foreach (var thing in words)
+                    {
+                        if (thing == word)
+                        {
+                            return true;
+                        }
+                    }
+                    return false;
+                default:
+                    Console.WriteLine("What did you do? You weren't supposed to do that!");
+                    break;
+            }
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if (num < 2)
+            {
+                return false;
+            }
+
+
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            int[] uniques = new int[str.Length];
+            int index = -1;
+
+            for (int i = 0; i < uniques.Length; i++)
+            {
+                uniques[i] = i;
+            }
+
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                for (int y = i + 1; y < str.Length; y++)
+                {
+                    if(str[i] == str[y])
+                    {
+                        uniques[i] = str.Length + 1;
+                        uniques[y] = str.Length + 1;
+                    }
+                }
+            }
+
+            foreach (var num in uniques)
+            {
+                if (num != str.Length + 1)
+                {
+                    index = num;
+                }
+            }
+            return index;
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            int count = 1;
+            int finalCount = 1;
+            int temp = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (temp == numbers[i])
+                {
+                    count++;
+                    if (count > finalCount)
+                    {
+                        finalCount = count;
+                    }
+                }
+                else
+                {
+                    count = 1;
+                    temp = numbers[i];
+                }
+            }
+            return finalCount;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            List<double> temp = new List<double>();
+            if (elements == null || n < 1)
+            {
+                return temp.ToArray();
+            }
+
+            for (int i = n-1; i < elements.Count; i += n)
+            {
+                temp.Add(elements[i]);
+            }
+            return temp.ToArray();
         }
     }
 }
